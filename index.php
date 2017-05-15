cc491bf386122c47586220fffa2a4249a01abd39
CeasarOu <cyclex@163.com>
2017-05-15 17:46:47 +0800

    Initial commit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	  $useragent = addslashes($_SERVER['HTTP_USER_AGENT']);
-        if(strpos($useragent, 'MicroMessenger') === false && strpos($useragent, 'Windows Phone') === false ){
-        	include '2.html';
-       }else{
-       	include 'zhen.html';
-       }
-
-?>
